0827e5e7f0d7c8752d21d2d58827248210c0cf44
Andres Davila <andresdavilasanchez@gmail.com>
2025-05-06 17:57:34 -0500

    Middle form fields added

--- masterview-form/src/App.jsx
+++ masterview-form/src/App.jsx
@@ -102,7 +102,7 @@ function App() {
             </div>
 
             {/* NOTIFY PARTY / INTERMEDIATE CONSIGNEE */}
-            <div className="row g-0 mt-0">
+            <div className="row g-0 mt-4">
               <div className="col-12">
                 <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
                   Notify Party / Intermediate Consignee
@@ -145,6 +145,118 @@ function App() {
             />
           </div>
         </div>
+
+        {/* INITIAL CARRIAGE BY (MODE)* y PLACE OF INITIAL RECEIPT* */}
+        <div className="row mt-4">
+          <div className="col-3">
+            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+              INITIAL CARRIAGE BY (MODE)*
+            </label>
+            <input
+              type="text"
+              className="form-control mt-0"
+              name="initialCarriageMode"
+              placeholder="Enter mode of initial carriage"
+            />
+          </div>
+          <div className="col-3">
+            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+              PLACE OF INITIAL RECEIPT*
+            </label>
+            <input
+              type="text"
+              className="form-control mt-0"
+              name="initialReceiptPlace"
+              placeholder="Enter place of initial receipt"
+            />
+          </div>
+        </div>
+
+        {/* VESSEL/VOY + PORT OF LOADING + LOADING PIER/TERMINAL + ORIGINAL(S) TO BE RELEASED AT */}
+        <div className="row mt-4">
+          <div className="col-3">
+            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+              VESSEL VOY
+            </label>
+            <input
+              type="text"
+              className="form-control mt-0"
+              name="vesselVoy"
+              placeholder="Enter vessel voy"
+            />
+          </div>
+          <div className="col-3">
+            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+              PORT OF LOADING
+            </label>
+            <input
+              type="text"
+              className="form-control mt-0"
+              name="portOfLoading"
+              placeholder="Enter port of loading"
+            />
+          </div>
+          <div className="col-3">
+            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+              LOADING PIER/TERMINAL
+            </label>
+            <input
+              type="text"
+              className="form-control mt-0"
+              name="loadingPier"
+              placeholder="Enter loading pier/terminal"
+            />
+          </div>
+          <div className="col-3">
+            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+              ORIGINAL(S) TO BE RELEASED AT
+            </label>
+            <input
+              type="text"
+              className="form-control mt-0"
+              name="originalsRelease"
+              placeholder="Enter location of release"
+            />
+          </div>
+        </div>
+
+        {/* PORT OF DISCHARGE + PORT OF DELIVERY */}
+<div className="row mt-4">
+  <div className="col-3">
+    <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+      PORT OF DISCHARGE
+    </label>
+    <input
+      type="text"
+      className="form-control mt-0"
+      name="portOfDischarge"
+      placeholder="Enter port of discharge"
+    />
+  </div>
+  <div className="col-3">
+    <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+      PORT OF DELIVERY
+    </label>
+    <input
+      type="text"
+      className="form-control mt-0"
+      name="portOfDelivery"
+      placeholder="Enter port of delivery"
+    />
+  </div>
+
+  <div className="col-6">
+    <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
+      TYPE OF MOVE
+    </label>
+    <input
+      type="text"
+      className="form-control mt-0"
+      name="typeOfMove"
+      placeholder="Enter type of move"
+    />
+  </div>
+</div>
 
         {/* Botón */}
         <div className="mb-3 mt-4">
