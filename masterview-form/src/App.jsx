65ed839108008741ee56980b26fdfec26e3c49c0
Andres Davila <andresdavilasanchez@gmail.com>
2025-05-07 17:20:25 -0500

    Unneccessary form fields deleted

--- masterview-form/src/App.jsx
+++ masterview-form/src/App.jsx
@@ -27,11 +27,6 @@ function App() {
               Booking Number
             </label>
           </div>
-          <div className="col-3">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-              Bill of Lading No.
-            </label>
-          </div>
         </div>
 
         {/* Campos SHIPPER/EXPORT + BOOKING/BILL OF LADING */}
@@ -53,26 +48,6 @@ function App() {
                   className="form-control mt-0"
                   name="bookingNumber"
                   placeholder="Enter booking number"
-                />
-              </div>
-              <div className="col-6 mb-2">
-                <input
-                  type="text"
-                  className="form-control mt-0"
-                  name="billOfLading"
-                  placeholder="Enter bill of lading no."
-                />
-              </div>
-              <div className="col-12">
-                <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-                  Export References
-                </label>
-                <textarea
-                  className="form-control mt-0"
-                  name="exportReferences"
-                  rows="4"
-                  placeholder="Enter export references"
-                  style={{ resize: 'none' }}
                 />
               </div>
             </div>
@@ -139,46 +114,6 @@ function App() {
               </div>
             </div>
           </div>
-
-          {/* FORWARDING AGENT */}
-          <div className="col-6">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-              Forwarding Agent
-            </label>
-            <textarea
-              className="form-control mt-0"
-              name="forwardingAgent"
-              rows="10"
-              placeholder="References F.M. C. No."
-              style={{ resize: 'none' }}
-            />
-          </div>
-        </div>
-
-        {/* INITIAL CARRIAGE BY (MODE)* y PLACE OF INITIAL RECEIPT* */}
-        <div className="row mt-4">
-          <div className="col-3">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-              Initial Carriage by (Mode)*
-            </label>
-            <input
-              type="text"
-              className="form-control mt-0"
-              name="initialCarriageMode"
-              placeholder="Enter mode of initial carriage"
-            />
-          </div>
-          <div className="col-3">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-              Place of Initial Receipt*
-            </label>
-            <input
-              type="text"
-              className="form-control mt-0"
-              name="initialReceiptPlace"
-              placeholder="Enter place of initial receipt"
-            />
-          </div>
         </div>
 
         {/* VESSEL/VOY + PORT OF LOADING + LOADING PIER/TERMINAL + ORIGINAL(S) TO BE RELEASED AT */}
@@ -205,28 +140,6 @@ function App() {
               placeholder="Enter port of loading"
             />
           </div>
-          <div className="col-3">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-              Loading Pier/Terminal
-            </label>
-            <input
-              type="text"
-              className="form-control mt-0"
-              name="loadingPier"
-              placeholder="Enter loading pier/terminal"
-            />
-          </div>
-          <div className="col-3">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
-              Original(s) to be released
-            </label>
-            <input
-              type="text"
-              className="form-control mt-0"
-              name="originalsRelease"
-              placeholder="Enter location of release"
-            />
-          </div>
         </div>
 
         {/* PORT OF DISCHARGE + PORT OF DELIVERY */}
@@ -240,28 +153,6 @@ function App() {
               className="form-control rounded-bottom"
               name="portOfDischarge"
               placeholder="Enter port of discharge"
-            />
-          </div>
-          <div className="col-3">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0 rounded-top">
-              Port of Delivery
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-bottom"
-              name="portOfDelivery"
-              placeholder="Enter port of delivery"
-            />
-          </div>
-          <div className="col-6">
-            <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0 rounded-top">
-              Type of Move
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-bottom"
-              name="typeOfMove"
-              placeholder="Enter type of move"
             />
           </div>
         </div>
@@ -407,7 +298,7 @@ function App() {
               Total Pkgs
             </label>
           </div>
-          <div className="col-6">
+          <div className="col-5">
             <label className="form-label fw-bold border p-2 d-block bg-light text-dark text-start mb-0">
               Description of Packages and Goods
             </label>
@@ -436,7 +327,7 @@ function App() {
             />
           </div>
 
-          <div className="col-6">
+          <div className="col-5">
             <textarea
               className="form-control mt-0"
               name="shipper"
